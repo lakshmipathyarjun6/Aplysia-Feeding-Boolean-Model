507ae5815acf4c19d9f6e149d26433c9a7e8d1b0
quantumRAV <quantumRAV@gmail.com>
2020-12-02 14:32:57 -0500

    Added switch in code to change between pure Matlab and Simulink implementations: USE_PURE_MATLAB_BIOMECHANICS

--- TestAplysiaClass.m
+++ TestAplysiaClass.m
@@ -6,11 +6,13 @@ xlimits = [0 40];
 
 aplysia = AplysiaSimulink();
 simFileName = "Test"
-aplysia.switchBehavior = 1;   %only behavior_1 is simulated 
+aplysia.switchBehavior = 0;   %only behavior_1 is simulated 
 aplysia.behavior_1 = double(FeedingBehavior.swallow);
 aplysia.behavior_2 = double(FeedingBehavior.reject);
 aplysia.t_switch = 20; %time to switch
 aplysia.stoptime = 40;
+
+USE_PURE_MATLAB_BIOMECHANICS = 0; %1 = use pure matlab biomechanics block.  0 = use simulink based biomechanics implementation.
 
 aplysia = aplysia.runSimulation();
 aplysia.generatePlots(['Swallow_' suffix],xlimits);
