165f30f614e7b8c0051159cc86e8c9d660db6115
quantumRAV <quantumRAV@gmail.com>
2020-12-04 13:48:17 -0500

    integrated Hill Type I2 muscle model.  Performing initial tests
    Note the following:
    
    To match the general characteristics of the original force characteristics, the input muscle length l_mt to the Hill-type muscle model was scaled and shifted as follows:
    (1-(x_gh)/k)*l_mto = l_mt
    
    x_gh is in range [0,1].
    k = 2.68 because active + passive I2 force produces 0 force at 0.627 (normalized).  So since F_I2 = 0 when x_gh =1, then based on equation above:
    
    xgh = k(1-l_mt/l_mto)
    1 = k(1-0.673)
    k = 2.68

---
+++ I2_Hill_Type_Muscle_Model.m
@@ -0,0 +1,37 @@
+classdef I2_Hill_Type_Muscle_Model
+    properties
+        I2_struct
+    end
+    
+    methods
+        
+        function obj = I2_Hill_Type_Muscle_Model()
+            
+            obj.I2_struct.tau=2.45;
+            obj.I2_struct.beta=0.703;
+            obj.I2_struct.a0=0.165;
+            obj.I2_struct.g_scale=2.0;
+            obj.I2_struct.L_st=0; %[m]
+            obj.I2_struct.F0=150*10/1000; %[N] Maximum isometric Force
+            obj.I2_struct.L_mto=18.3/1000; %[m]
+            obj.I2_struct.L_0 = 0.858*obj.I2_struct.L_mto; %[m]
+
+            obj.I2_struct.v0=0;
+            obj.I2_struct.L_m_0=obj.I2_struct.L_mto; %contractile element initial length
+            obj.I2_struct.K_t=5*obj.I2_struct.F0/obj.I2_struct.L_0;
+
+            obj.I2_struct.activation_freq=20; %[Hz] frequency of sinusoid input
+
+            obj.I2_struct.v_max=0.007; % [m s^-1] %shortening velocity
+            obj.I2_struct.k_I2 = 2.68; %scale factor to span l_mt so that it matches x_gh from l/l_mt0 in range [0,1] 
+            
+            
+            assignin('base','I2_struct',obj.I2_struct)
+            
+        end
+        
+    end
+   
+    
+    
+end
--- TestAplysiaClass.m
+++ TestAplysiaClass.m
@@ -2,22 +2,34 @@ close all
 clear all
 
 suffix = '_8_9_2020';
-xlimits = [0 80];
+xlimits = [0 40];
 
+
+%set initialization parameters for simulink model
 aplysia = AplysiaSimulink();
 simFileName = "Test"
 aplysia.switchBehavior = 0;   %only behavior_1 is simulated 
-aplysia.behavior_1 = double(FeedingBehavior.reject);
+aplysia.behavior_1 = double(FeedingBehavior.swallow);
 aplysia.behavior_2 = double(FeedingBehavior.reject);
 aplysia.t_switch = 20; %time to switch
 
-USE_PURE_MATLAB_BIOMECHANICS = 1; %1 = use pure matlab biomechanics block.  0 = use simulink based biomechanics implementation.
-inargs.TimeStep=0.050;
+inargs.TimeStep=0.001;
 inargs.EndTime=xlimits(2);
+% end set initialization parameters
 
+%Switches
+USE_PURE_MATLAB_BIOMECHANICS = 0; %1 = use pure matlab biomechanics block.  0 = use simulink based biomechanics implementation.
+USE_YU_I2_HILL_TYPE_MUSCLE_MODEL = 1; %1 = use Yu Hill Type Muscle Model.  0 = use original force model
+%end switches
 
+%initialize I2 Hill TYpe Muscle Model parameters
+HTM_I2 = I2_Hill_Type_Muscle_Model();
+%end initialize I2 Hill TYpe Muscle Model parameters
+
+%run simulink model
 aplysia = aplysia.runSimulation(inargs);
 aplysia.generatePlots(['Swallow_' suffix],xlimits);
+%end run simulink model
 
 
 %%-----------------
@@ -27,7 +39,7 @@ aplysia_MATLAB = AplysiaFeeding_MATLAB(inargs );
 
 
 t=aplysia_MATLAB.StartingTime:aplysia_MATLAB.TimeStep:aplysia_MATLAB.EndTime;
-aplysia_MATLAB = aplysia_MATLAB.setSensoryStates('reject');
+aplysia_MATLAB = aplysia_MATLAB.setSensoryStates('swallow');
 aplysia_MATLAB = aplysia_MATLAB.runSimulation();
 
 figure()
@@ -37,23 +49,23 @@ plot(t,aplysia_MATLAB.force_on_object,'b--','LineWidth',2); %MATLAB
 
 
 
-%%--------------
-%%Debug
-%%--------------
-fieldN = fieldnames(aplysia.outStruct);
-fieldN_MATLAB = fieldnames(aplysia_MATLAB);
-figure()
-for n = 1:length(fieldN_MATLAB)
-    val=aplysia_MATLAB.(fieldN_MATLAB{n});
-    if length(val)==1
-        val = repmat(val, length(t), 1);
-    end
-    plot(t,val','b-');
-    hold on
-    if isfield(aplysia.outStruct, fieldN_MATLAB{n})
-        plot(aplysia.outStruct.(fieldN_MATLAB{n}).Time,aplysia.outStruct.(fieldN_MATLAB{n}).Data,'k','LineWidth',2);
-    end
-    title(fieldN_MATLAB{n});
-    hold off
-    shg
-end
+% %%--------------
+% %%Debug
+% %%--------------
+% fieldN = fieldnames(aplysia.outStruct);
+% fieldN_MATLAB = fieldnames(aplysia_MATLAB);
+% figure()
+% for n = 1:length(fieldN_MATLAB)
+%     val=aplysia_MATLAB.(fieldN_MATLAB{n});
+%     if length(val)==1
+%         val = repmat(val, length(t), 1);
+%     end
+%     plot(t,val','b-');
+%     hold on
+%     if isfield(aplysia.outStruct, fieldN_MATLAB{n})
+%         plot(aplysia.outStruct.(fieldN_MATLAB{n}).Time,aplysia.outStruct.(fieldN_MATLAB{n}).Data,'k','LineWidth',2);
+%     end
+%     title(fieldN_MATLAB{n});
+%     hold off
+%     shg
+% end
